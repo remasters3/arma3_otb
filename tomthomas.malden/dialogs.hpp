d2b106b6a5718f8a59c5af9bebcdf944283b01b0
gerryfranchetti@gmail.com <gerryfranchetti@gmail.com>
2020-03-15 15:56:54 +0000

    15/03/2020 - 15:56:54.56

--- tomthomas.malden/dialogs.hpp
+++ tomthomas.malden/dialogs.hpp
@@ -293,7 +293,7 @@ class GPF_RSCButton800: RscButton
 	y = 0.665 * safezoneH + safezoneY;
 	w = 0.0928125 * safezoneW;
 	h = 0.022 * safezoneH;
-	action = "closeDialog 0;{ if (!isplayer) then {vehicle _x SetDamage 1;};} Foreach AllUnits;";
+	action = "closeDialog 0;{ if (_x != player) then {vehicle _x SetDamage 1;};} Foreach AllUnits;";
 	
 };
 class GPF_RSCButton900: RscButton
